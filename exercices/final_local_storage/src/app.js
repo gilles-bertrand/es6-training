d96b5854a3c2b03ee1c61ef6b68f511c1c169c13
gilles-bertrand <gilles@triptyk.eu>
2018-07-09 21:32:52 +0200

    add localforage

--- exercices/final_local_storage/src/app.js
+++ exercices/final_local_storage/src/app.js
@@ -1,5 +1,14 @@
 /* global window document $*/
 
+function getBase64(file) {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => resolve(reader.result);
+    reader.onerror = error => reject(error);
+  });
+}
+
 const show = elem => {
   elem.style.display = "block";
 };
@@ -19,9 +28,28 @@ const addInteractivity = () => {
   addAnimalButton.addEventListener("click", () => {
     changeScreen("addForm");
   });
+
+  const addForm = document.querySelector("#addForm");
+  const name = document.querySelector("#name");
+  const lat = document.querySelector("#latitude");
+  const lon = document.querySelector("#longitude");
+  const file = document.querySelector("#file");
+  addForm.addEventListener("submit", e => {
+    e.preventDefault();
+    getBase64(file.files[0]).then(result => {
+      const record = {
+        name: name.value,
+        lat: lat.value,
+        lon: lon.value,
+        image: result
+      };
+    });
+  });
 };
 
 const init = () => {
+  //init localstorage
+
   mapboxgl.accessToken =
     "pk.eyJ1IjoidHJpcHR5ayIsImEiOiJjampjemtuZjEzcjQwM3dtbnN0OWY1ZjNiIn0.GikR8T10cdWqqaim5MJUXg";
   const carte = new mapboxgl.Map({
